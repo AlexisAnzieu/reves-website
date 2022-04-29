e4d0ccfe978f4f02367bad5e7ee7180c7e8691bc
alexis anzieu <alexis.anzieu@ssense.com>
2022-04-29 13:22:53 -0400

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,26 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbarx";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
+            <Head>
+                <title>Reve.s</title>
+                <meta property="og:title" content="Reve.s" key="ogtitle" />
+                <link rel="shortcut icon" href="/favicon.png" />
+                <meta
+                    property="og:description"
+                    content="S'épanouir ensemble"
+                    key="ogdesc"
+                />
+                <meta
+                    property="og:image"
+                    content="https://reve-s.net/favicon.png"
+                    key="ogpic"
+                />
+            </Head>
             <Navbar></Navbar>
             <Component {...pageProps} />
         </ChakraProvider>
