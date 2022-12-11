a88cc5acbf8bcc79238cd10b1e95a3408ba5d389
alexis anzieu <alexis.anzieu@ssense.com>
2022-12-11 11:50:08 -0500

    plug to directus

--- pages/associations/index.tsx
+++ pages/associations/index.tsx
@@ -13,8 +13,19 @@ import {
     SimpleGrid,
     Box,
 } from "@chakra-ui/react";
+import { directus } from "../../helpers/directus";
 
-export default function Sessions() {
+export async function getStaticProps() {
+    const { data: associations } = await directus
+        .items("associations")
+        .readByQuery();
+
+    return {
+        props: { associations },
+    };
+}
+
+export default function Associations({ associations }: any) {
     return (
         <Container p={"30px"}>
             <Heading size={"lg"} pb={5}>
@@ -28,49 +39,47 @@ export default function Sessions() {
                 spacing={4}
                 templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
             >
-                <Card shadow={"xl"} maxW="sm">
-                    <CardBody>
-                        <Stack mt="6" spacing="3">
-                            <Heading size="md">Santropol Roulant</Heading>
-                            <Text noOfLines={5}>
-                                Santropol Roulant est un centre alimentaire et
-                                une communauté intergénérationnelle. En faisant
-                                pousser de la nourriture, en la préparant, et en
-                                la livrant aux Montréalaises et Montréalais,
-                                nous créons un continuum de services inspirants
-                                et engageants au bénéfice de la communauté, et
-                                nous renforçons le tissu social montréalais en
-                                faisant grandir la sécurité alimentaire en ville
-                                et l’inclusion sociale.{" "}
-                            </Text>
-                            <Link
-                                textDecoration={"underline"}
-                                href="https://santropolroulant.org/fr/"
-                            >
-                                En savoir plus
-                            </Link>
-                        </Stack>
-                    </CardBody>
-                    <Divider />
-                    <CardFooter>
-                        <ButtonGroup spacing="2">
-                            <Link
-                                style={{ textDecoration: "none" }}
-                                href="/associations/santropol-roulant"
-                            >
-                                <Button
-                                    _hover={{
-                                        backgroundColor: "orange.400",
-                                    }}
-                                    color={"white"}
-                                    colorScheme="cyan"
-                                >
-                                    Voir les sessions
-                                </Button>
-                            </Link>
-                        </ButtonGroup>
-                    </CardFooter>
-                </Card>
+                {associations.map((association: any) => {
+                    return (
+                        <Card key={association.slug} shadow={"xl"} maxW="sm">
+                            <CardBody>
+                                <Stack mt="6" spacing="3">
+                                    <Heading size="md">
+                                        {association.title}
+                                    </Heading>
+                                    <Text noOfLines={5}>
+                                        {association.description}
+                                    </Text>
+                                    <Link
+                                        textDecoration={"underline"}
+                                        href={association.websiteUrl}
+                                    >
+                                        En savoir plus
+                                    </Link>
+                                </Stack>
+                            </CardBody>
+                            <Divider />
+                            <CardFooter>
+                                <ButtonGroup spacing="2">
+                                    <Link
+                                        style={{ textDecoration: "none" }}
+                                        href={`/associations/${association.slug}`}
+                                    >
+                                        <Button
+                                            _hover={{
+                                                backgroundColor: "orange.400",
+                                            }}
+                                            color={"white"}
+                                            colorScheme="cyan"
+                                        >
+                                            Voir les sessions
+                                        </Button>
+                                    </Link>
+                                </ButtonGroup>
+                            </CardFooter>
+                        </Card>
+                    );
+                })}
                 <Card maxW="sm" shadow={"sm"}>
                     <CardBody>
                         <Stack mt="6" spacing="3">
